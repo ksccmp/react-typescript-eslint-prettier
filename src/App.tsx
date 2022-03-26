aa4663467addc394da40cb3f317426cb18d422ef
김성찬 <tjdcksdl00@naver.com>
2022-03-26 14:17:39 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const App = (): JSX.Element => {
+    return (
+        <div>
+            <p>Hello, React!</p>
+            <p>Hello, Typescript!</p>
+        </div>
+    );
+};
+
+export default App;
